a656965dbb59592ed192abc29f715ec23a0e99f9
moro <moronatural@gmail.com>
2009-01-13 17:22:51 +0900

    fix test bug.

--- spec/scope_do/named_acl_spec.rb
+++ spec/scope_do/named_acl_spec.rb
@@ -12,6 +12,7 @@ describe ScopeDo::NamedAcl do
       named_acl :blogs
     end
 
+    User.delete_all
     u = Factory(:user)
     b = Factory(:blog)
     b.accessibilities.map(&:group).first.users << u
