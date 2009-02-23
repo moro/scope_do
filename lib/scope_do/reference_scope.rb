43de021394387cc2606cc114647b14d1608bfefd
moro <moronatural@gmail.com>
2009-02-23 19:38:13 +0900

    1 path

--- lib/scope_do/reference_scope.rb
+++ lib/scope_do/reference_scope.rb
@@ -5,7 +5,17 @@ module ScopeDo
     end
 
     module ClassMethods
-      def reference_scope(*args)
+      def reference_scope(association, scope, options = {})
+        scope_name = options[:as] || "#{association}_#{scope}".to_sym
+
+        ref = self.reflections[association]
+        impl = lambda{|arg|
+          base = ref.active_record
+          scope = ref.klass.send(scope, arg)
+          jd = ActiveRecord::Associations::ClassMethods::InnerJoinDependency.new(base, association, scope.proxy_options[:joins])
+          scope.proxy_options.dup.update(:joins => jd.join_associations.map{|j| j.association_join }.join(" "))
+        }
+        named_scope scope_name, impl
       end
     end
   end
--- spec/scope_do/references_spec.rb
+++ spec/scope_do/references_spec.rb
@@ -7,18 +7,22 @@ require 'scope_do/reference_scope'
 
 describe ScopeDo::ReferenceScope do
   before(:all) do
+    ::Publication.class_eval do
+      named_scope :since, lambda{|t| {:conditions => ["#{quoted_table_name}.published_at < ?", t]} }
+    end
     ::Entry.class_eval do
       scope_do :reference_scope
-      reference_scope :publication, :since, :as => :published_since
+      reference_scope :publication, :since, :as => :published
     end
 
     [Entry, Publication].each(&:delete_all)
 
-    draft, *published = 5.times{ Factory(:entry) }
+    draft, *published = (0...5).map{|i| Factory(:entry) }
     t = 3.hours.ago
     published.each{|e| e.create_publication(:published_at => t) }
   end
 
-  it{ :ok }
+  it{ Publication.since(Time.now).count.should == 4 }
+  it{ Entry.published(Time.now).count.should == 4 }
 end
 
